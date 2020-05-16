ce50accff71e65d2ef3f1bc77851023c8264311b
hamidzargar <hzargar2@gmail.com>
2020-05-16 12:20:05 -0400

    added url to dictionary

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-timetable_urls_dict = {'Summer': 'https://studentservices.uwo.ca/secure/timetables/SummerTT/ttindex.cfm',
-                       'Fall/Winter': 'https://studentservices.uwo.ca/secure/timetables/mastertt/ttindex.cfm'
-                       }
+urls_dict = {'Summer': 'https://studentservices.uwo.ca/secure/timetables/SummerTT/ttindex.cfm',
+             'Fall/Winter': 'https://studentservices.uwo.ca/secure/timetables/mastertt/ttindex.cfm',
+             'Student_Center_Login_Page': 'https://student.uwo.ca/psp/heprdweb/?cmd=login'
+             }
